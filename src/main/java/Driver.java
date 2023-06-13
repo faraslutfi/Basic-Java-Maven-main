6f5a9471887af73b9158eee4dc6be3e00d4f9f9c
faraslutfi <127936502+faraslutfi@users.noreply.github.com>
2023-06-13 23:36:30 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,5 +3,9 @@ public class Driver {
         int number = 12; // Ganti bilangan sesuai dengan yang ingin Anda cek
         String result = NumberChecker.checkNumber(number);
         System.out.println("Bilangan " + number + " adalah " + result);
+         System.out.println("Bilangan " + number + " adalah " + result);
+         System.out.println("Bilangan " + number + " adalah " + result);
+         System.out.println("Bilangan " + number + " adalah " + result);
+        
     }
 }
